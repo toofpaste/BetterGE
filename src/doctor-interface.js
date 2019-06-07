3369bd3073dacb44347163b3d786e885d15af062
Michael Larragueta <m.larragueta@yahoo.com>
2019-06-07 11:37:43 -0700

    Add check list for symtpoms

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -2,15 +2,25 @@ import {DoctorService} from "./doctor-service";
 import './styles.css';
 
 $(document).ready(function() {
+    let docService = new DoctorService();
+
+    docService.getList()
+      .then(function(response) {
+          let body = JSON.parse(response);
+          for(let d = 0; d < body.data.length; d++){
+          $("#search").append(`<option value='${body.data[d].name}'>${body.data[d].name}</option>`);
+          }
+      })
     $('#doctorSearch').click(function() {
+        $(".errors").empty();
+        $("#title").empty();
+        $("#docs").empty();
         let search = $('#search').val();
         $('#search').val("");
         let name = $('#name').val();
         $('#name').val("");
-        let docService = new DoctorService();
 
-
-        docService.docCall(search, name)
+       docService.docCall(search, name)
             .then(function(response) {
                 let body = JSON.parse(response);
                 let nameHold = [];
@@ -19,18 +29,21 @@ $(document).ready(function() {
                 let streetHold = [];
                 let zipHold = [];
                 let phoneHold = [];
-                for(var i = 0; i < body.data[0].practices.length; i++){
-                    nameHold.push(body.data[0].practices[i].name);
-                    cityHold.push(body.data[0].practices[i].visit_address.city);
-                    stateHold.push(body.data[0].practices[i].visit_address.state);
-                    streetHold.push(body.data[0].practices[i].visit_address.street);
-                    zipHold.push(body.data[0].practices[i].visit_address.zip);
-                    phoneHold.push(body.data[0].practices[i].phones[0].number);
-                    $("#docs").append(`<div id='doc${i}'></div>`);
-                }
-                for(var x = 0; x < nameHold.length; x++){
-                    $(`#doc${x}`).html(`Name: ${nameHold[x]}<br>Address: ${streetHold[x]}, ${cityHold[x]}, ${stateHold[x]}, ${zipHold[x]} <br> Phone Number: ${phoneHold[x]}`);
-                }
+                if(body.meta.total > 0) {
+                    $("#title").html(`<h2>Doctors found that can help with: ${search}</h2>`);
+                    for (var i = 0; i < body.data[0].practices.length; i++) {
+                        nameHold.push(body.data[0].practices[i].name);
+                        cityHold.push(body.data[0].practices[i].visit_address.city);
+                        stateHold.push(body.data[0].practices[i].visit_address.state);
+                        streetHold.push(body.data[0].practices[i].visit_address.street);
+                        zipHold.push(body.data[0].practices[i].visit_address.zip);
+                        phoneHold.push(body.data[0].practices[i].phones[0].number);
+                        $("#docs").append(`<div id='doc${i}'></div>`);
+                    }
+                    for (var x = 0; x < nameHold.length; x++) {
+                        $(`#doc${x}`).html(`Name: ${nameHold[x]}<br>Address: ${streetHold[x]}, ${cityHold[x]}, ${stateHold[x]}, ${zipHold[x]} <br> Phone Number: ${phoneHold[x]}`);
+                    }
+                }else $(".errors").html(`<h2>No doctors found with the name ${name} and the symptom of ${search}</h2>`);
                // return weatherService.hourCall(city);
             })
     });
--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,10 +1,27 @@
 //let weatherKey = "e5b2f22a390cc27d39eda60d69995f4a";
 let docKey = "87ba8f96382f5e689a7ae52826597cea";
 export class DoctorService {
+    getList(){
+        return new Promise(function(resolve, reject) {
+            let request = new XMLHttpRequest();
+            let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${docKey}`;
+
+            request.onload = function() {
+                if (this.status === 200) {
+                    resolve(request.response);
+                } else {
+                    reject(Error(request.statusText));
+                }
+            }
+
+            request.open("GET", url, true);
+            request.send();
+        });
+    }
     docCall(search, name) {
         return new Promise(function(resolve, reject) {
             let request = new XMLHttpRequest();
-            let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&location=45.5155%2C-122.6793%2C100&skip=0&limit=10&user_key=${docKey}`;
+            let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${search}&location=45.5155%2C-122.6793%2C100&skip=0&limit=10&user_key=${docKey}`;
 
             request.onload = function() {
                 if (this.status === 200) {
